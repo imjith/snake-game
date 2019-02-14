8e8bc42c4527fe4a371a49af43fd49acba372a74
ramsheed <imjith@gmail.com>
2019-02-15 00:43:09 +0400

    re-write the logic

--- src/components/GameCanvas.vue
+++ src/components/GameCanvas.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    @keyup.right="moveRight"
-    @keyup.up="moveUp"
-    @keyup.down="moveDown"
-    @keyup.left="moveLeft"
+    @keyup.right="move(1)"
+    @keyup.up="move(2)"
+    @keyup.down="move(-2)"
+    @keyup.left="move(-1)"
     class="game-canvas"
     :style="{'max-width': (w*size)+'px'}"
   >
     <div
       v-for="n in size*size"
       :key="n"
-      :class="['pixel', snake[n]]"
+      :class="['pixel', snake[n]==0?'white':'snake']"
       :style="{width:w+'px',height:w+'px'}"
     ></div>
 
@@ -21,28 +21,25 @@
 <script>
 export default {
   name: "GameCanvas",
-  props: {
-    msg: String
-  },
+  props: {},
 
   data() {
     return {
-      size: 10,
-      w: 20,
-      snake: Array(101).fill("white"),
+      size: 40,
+      w: 10,
+      snake: Array(1601).fill(0),
       defHead: 44,
       defTail: 42,
       head: 44,
       tail: 42,
-      movingLef: false,
-      movingRight: false,
-      movingUp: false,
-      movingDown: false
+      curMovement: 1,
+      isMoving: false,
+      task: null
     };
   },
   mounted: function() {
     for (let i = this.defTail; i <= this.defHead; i++) {
-      this.snake.splice(i, 1, "snake");
+      this.snake.splice(i, 1, 1);
     }
   },
   methods: {
@@ -60,181 +57,88 @@ export default {
     },
     gameOver() {
       alert("Game over");
-      this.snake = Array(101).fill("white");
+      this.snake = Array(1601).fill(0);
       this.head = this.defHead;
       this.tail = this.defTail;
+      this.isMoving = false;
+      this.curMovement = 1;
+      clearInterval(this.task);
+      this.task = null;
       for (let i = this.defTail; i <= this.defHead; i++) {
-        this.snake.splice(i, 1, "snake");
+        this.snake.splice(i, 1, 1);
       }
     },
-    moveRight() {
-      let tailCol = this.getTailCol();
-      let headCol = this.getHeadCol();
-      if (headCol != 0) {
-        let headRow = this.getHeadRow();
-        let tailRow = this.getTailRow();
-        if (this.head > this.tail || tailRow != headRow) {
-          this.snake.splice(this.tail, 1, "white");
-
-          if (tailCol == headCol) {
-            if (this.head > this.tail) {
-              this.tail += this.size;
-            } else {
-              this.tail -= this.size;
-            }
-          } else {
-            if (headRow > tailRow) {
-              if (this.snake[this.tail + this.size] != "white") {
-                this.tail += this.size;
-              } else {
-                this.tail += 1;
-              }
-            } else {
-              if (headCol > tailCol) {
-                this.tail += 1;
-              } else {
-                this.tail -= 1;
-              }
-            }
-          }
-          this.head += 1;
-          this.snake.splice(this.head, 1, "snake");
-          //this.snake.splice(this.tail, 1, "snake");
-        }
-      } else {
-        this.gameOver();
+    isGameOver(e) {
+      let row = Math.ceil(this.head / this.size);
+      let col = this.head % this.size;
+      return (
+        (e == 1 && col == 0) ||
+        (e == -1 && col == 1) ||
+        (e == 2 && row == 1) ||
+        (e == -2 && row == this.size)
+      );
+    },
+    move(e) {
+      console.log(`event :${e}`);
+      if (null != this.task) {
+        clearInterval(this.task);
+      }
+      if (e != -this.curMovement) {
+        console.log(`Moving : ${e}`);
+        this.isMoving = true;
+        this.startRunning(e);
+        this.curMovement = e;
       }
     },
-    moveLeft() {
-      let headRow = this.getHeadRow();
-      let headCol = this.getHeadCol();
-      if (headCol != 1) {
-        let tailRow = this.getTailRow();
-        if (this.head < this.tail || tailRow != headRow) {
-          let tailCol = this.getTailCol();
-
-          this.snake.splice(this.tail, 1, "white");
-          this.head -= 1;
-          if (tailCol == headCol) {
-            if (this.head > this.tail) {
-              this.tail += this.size;
-            } else {
-              this.tail -= this.size;
-            }
-          } else {
-            if (tailRow == headRow) {
-              this.tail -= 1;
-            } else if (headRow < tailRow && headCol > tailCol) {
-              this.tail += 1;
-            } else if (headRow < tailRow && headCol < tailCol) {
-              this.tail -= 1;
-            } else {
-              this.tail += this.size;
-            }
-          }
-
-          this.snake.splice(this.head, 1, "snake");
-          // this.snake.splice(this.tail, 1, "snake");
+    startRunning(e) {
+      this.task = setInterval(() => {
+        if (this.isGameOver(e)) {
+          this.gameOver();
+        } else {
+          this.moveSnake(e);
         }
-      } else {
-        this.gameOver();
-      }
+      }, 200);
     },
-    moveUp() {
-      let headRow = this.getHeadRow();
-      let tailRow = this.getTailRow();
-      if (headRow > 1) {
-        /*
-        invalid conditions
-        1. when head is large and both in same col
-        2. 
-        */
-        let tailCol = this.getTailCol();
-        let headCol = this.getHeadCol();
-        if (this.head < this.tail || tailCol != headCol) {
-          this.snake.splice(this.tail, 1, "white");
-
-          if (tailCol == headCol) {
-            this.tail -= this.size;
-          } else {
-            if (headRow == tailRow) {
-              if (headCol > tailCol) {
-                this.tail += 1;
-              } else {
-                this.tail -= 1;
-              }
-            } else {
-              if (headRow > tailRow && headCol > tailCol) {
-                this.tail += this.size;
-              } else if (headRow < tailRow && headCol > tailCol) {
-                if (this.snake[this.head + this.size] != "white") {
-                  this.tail += 1;
-                } else {
-                  this.tail -= this.size;
-                }
-              } else if (headRow < tailRow && headCol < tailCol) {
-                if (this.snake[this.head + this.size] != "white") {
-                  this.tail -= 1;
-                } else {
-                  this.tail -= this.size;
-                }
-              } else {
-                this.tail += 1;
-              }
-            }
-          }
-          this.head -= this.size;
-
-          //this.snake.splice(this.tail, 1, "snake");
-          this.snake.splice(this.head, 1, "snake");
-        }
-      } else {
-        this.gameOver();
-      }
+    moveSnake(e) {
+      this.moveTail(e);
+      this.moveHead(e);
     },
-    moveDown() {
-      let headRow = this.getHeadRow();
-      let tailRow = this.getTailRow();
-      if (headRow < this.size) {
-        /*
-        invalid conditions
-        1. when head is small and both in same col
-        2. 
-        */
-        let tailCol = this.getTailCol();
-        let headCol = this.getHeadCol();
-        if (
-          this.head > this.tail ||
-          (tailCol != headCol && this.snake[this.head + this.size] == "white")
-        ) {
-          this.snake.splice(this.tail, 1, "white");
-          if (tailCol == headCol) {
-            this.tail += this.size;
-          } else {
-            if (headRow == tailRow) {
-              this.tail += 1;
-            } else if (headCol > tailCol) {
-              if (headRow > tailRow) {
-                this.tail += this.size;
-              } else {
-                this.tail -= this.size;
-              }
-            } else {
-              if (headRow > tailRow) {
-                this.tail -= 1;
-              } else {
-                this.tail -= this.size;
-              }
-            }
-          }
-
-          this.head += this.size;
-          //this.snake.splice(this.tail, 1, "snake");
-          this.snake.splice(this.head, 1, "snake");
-        }
+    moveHead(e) {
+      if (e == 1) {
+        this.head += 1;
+      } else if (e == -1) {
+        this.head -= 1;
+      } else if (e == 2) {
+        this.head -= this.size;
       } else {
-        this.gameOver();
+        this.head += this.size;
       }
+      this.snake.splice(this.head, 1, 1);
+    },
+    moveTail(e) {
+      this.snake.splice(this.tail, 1, 0);
+      if (!this.isUpperEmpty(this.tail)) {
+        this.tail -= this.size;
+      } else if (!this.isRightEmpty(this.tail)) {
+        this.tail += 1;
+      } else if (!this.isLeftEmpty(this.tail)) {
+        this.tail -= 1;
+      } else {
+        this.tail += this.size;
+      }
+      this.snake.splice(this.tail, 1, 1);
+    },
+    isUpperEmpty(pos) {
+      return this.snake[pos - this.size] == 0;
+    },
+    isBottomEmpty(pos) {
+      return this.snake[pos + this.size] == 0;
+    },
+    isLeftEmpty(pos) {
+      return this.snake[pos - 1] == 0;
+    },
+    isRightEmpty(pos) {
+      return this.snake[pos + 1] == 0;
     }
   }
 };
@@ -243,7 +147,7 @@ export default {
 
 <style scoped>
 .pixel {
-  border: 1px solid lightblue;
+  border: 1px solid rgb(207, 229, 229);
 }
 .game-canvas {
   display: flex;
@@ -251,7 +155,7 @@ export default {
 }
 
 .white {
-  background-color: white;
+  background-color: rgb(207, 229, 229);
 }
 
 .snake {
